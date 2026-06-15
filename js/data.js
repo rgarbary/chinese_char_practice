40c1825f9d4c8ec6dbd2df79bb7fb1c0f952c0be
Robert Garbary <rgarbary@gmail.com>
2026-06-15 09:35:22 -0400

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,6 @@
+export const vocabulary = [
+  { character: "你好", pinyin: "ni hao", english: "hello" },
+  { character: "谢谢", pinyin: "xie xie", english: "thank you" },
+  { character: "再见", pinyin: "zai jian", english: "goodbye" },
+  { character: "水", pinyin: "shui", english: "water" }
+];
