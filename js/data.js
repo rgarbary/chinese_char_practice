13102fd94187fd92496f09ed99a575b482b9ec2d
Robert Garbary <rgarbary@gmail.com>
2026-06-15 09:52:55 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,7 @@
 export const vocabulary = [
   { character: "你好", pinyin: "nǐ hǎo", english: "hello",include: false },
   { character: "谢谢", pinyin: "xièxie", english: "thank you",include: false },
-  { character: "再见", pinyin: "zàijiàn", english: "goodbye or see you again",include: true },
-  { character: "水", pinyin: "shǔi", english: "water",include: true }
+  { character: "再见", pinyin: "zàijiàn", english: "goodbye OR see you again",include: true },
+  { character: "水", pinyin: "shǔi", english: "water",include: true },
+  { character: "到", pinyin: "dào", english: "arrive OR until ",include: true }
 ];
