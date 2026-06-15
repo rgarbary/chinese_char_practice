5d791190b48cf9554fd6ccb224c411a09c33a0cf
Robert Garbary <rgarbary@gmail.com>
2026-06-15 10:16:34 -0400

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,44 +1,35 @@
 export const UI = {
-  questionEl: document.getElementById("question"),
   typeEl: document.getElementById("questionType"),
-  inputEl: document.getElementById("answerInput"),
+  questionEl: document.getElementById("question"),
   feedbackEl: document.getElementById("feedback"),
   scoreEl: document.getElementById("score"),
 
-  showQuestion(q) {
-    this.typeEl.textContent = `Question type: ${q.type}`;
-    this.questionEl.textContent = q.value;
-    this.inputEl.value = "";
-    this.feedbackEl.textContent = "";
+  showChoices: function(choices, onSelect) {
+    const container = document.getElementById("choices");
+    container.innerHTML = "";
+
+    choices.forEach(choice => {
+      const div = document.createElement("div");
+      div.className = "choice";
+      div.textContent = choice;
+
+      div.onclick = () => onSelect(choice);
+
+      container.appendChild(div);
+    });
   },
 
-  showCorrect() {
+  showCorrect: function() {
     this.feedbackEl.textContent = "Correct!";
     this.feedbackEl.style.color = "green";
   },
 
-  showWrong(correctAnswer) {
-    this.feedbackEl.textContent = `Wrong. Answer: ${correctAnswer}`;
+  showWrong: function(answer) {
+    this.feedbackEl.textContent = `Wrong. Answer: ${answer}`;
     this.feedbackEl.style.color = "red";
   },
 
-  updateScore(score) {
+  updateScore: function(score) {
     this.scoreEl.textContent = `Score: ${score}`;
   }
-
-  showChoices(choices, onSelect) {
-  const container = document.getElementById("choices");
-  container.innerHTML = "";
-
-  choices.forEach(choice => {
-    const div = document.createElement("div");
-    div.className = "choice";
-    div.textContent = choice;
-
-    div.onclick = () => onSelect(choice);
-
-    container.appendChild(div);
-  });
-}
-  
 };
